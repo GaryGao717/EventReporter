5760122e7c1bf90a6cd0dae00cbe5fdb58179abe
GaryRoach <gaoye19910629@gmail.com>
2018-04-10 23:40:14 -0400

    firebase notification

--- app/src/main/java/com/laioffer/eventreporter/MyFirebaseMessagingService.java
+++ app/src/main/java/com/laioffer/eventreporter/MyFirebaseMessagingService.java
@@ -41,7 +41,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             }
 
         }
-        sendNotification("Send notification to start EventReporter");
+        sendNotification(remoteMessage);
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
@@ -59,7 +59,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     /**
      * Create and show a simple notification containing the received FCM message.
      */
-    private void sendNotification(String fcmmessage) {
+    private void sendNotification(RemoteMessage remoteMessage) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
@@ -72,9 +72,10 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         //Create Notification according to builder pattern
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this)
+                        .setLargeIcon(Utils.getBitmapFromURL(remoteMessage.getData().get("imgUri")))
                         .setSmallIcon(R.drawable.icon)
-                        .setContentTitle("FCM Message")
-                        .setContentText(fcmmessage)
+                        .setContentTitle(remoteMessage.getData().get("title"))
+                        .setContentText(remoteMessage.getData().get("description"))
                         .setAutoCancel(true)
                         .setSound(defaultSoundUri)
                         .setContentIntent(pendingIntent);
